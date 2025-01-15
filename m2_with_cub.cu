fb6ebe0263ad57377f2f352ca7b6c81c91b43f06
NIKHIL GAIKWAD <51406515+StepUpTransformer@users.noreply.github.com>
2025-01-15 21:17:31 +0100

    Update m2_with_cub.cu

--- m2_with_cub.cu
+++ m2_with_cub.cu
@@ -57,7 +57,7 @@ __global__ void softmax_1_cuda(float* output, float* softmax_out, int output_siz
 }
 
 // Function to initialize data arrays with specific formulas
-void initializeData(float* input, float W1, float* b1, float* W2, float* b2, int input_size, int hidden_size, int output_size) {
+void initializeData(float* input, float* W1, float* b1, float* W2, float* b2, int input_size, int hidden_size, int output_size) {
     for (int i = 0; i < input_size; ++i) {
         input[i] = i * 5e-10f;
     }
